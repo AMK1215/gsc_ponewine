96022ae25e43cda4c4e6a76323da1714350271e3
AMK1215 <aungmyokhaing1215@gmail.com>
2025-03-03 10:15:24 +0630

    Game list update slotmaker to gsc

--- app/Models/HotGame.php
+++ app/Models/HotGame.php
@@ -1,19 +1,17 @@
 <?php
 
-namespace App\Models\Admin;
+namespace App\Models;
 
 use App\Models\Admin\GameType;
 use App\Models\Admin\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class GameList extends Model
+class HotGame extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['game_type_id', 'product_id', 'name', 'code', 'image_url', 'click_count', 'status', 'hot_status'];
-
-
+    protected $fillable = ['code', 'name', 'click_count', 'game_type_id', 'product_id', 'image_url', 'status'];
 
     public function product()
     {
@@ -24,11 +22,4 @@ class GameList extends Model
     {
         return $this->belongsTo(GameType::class, 'game_type_id');
     }
-
-    public function getImgUrlAttribute()
-    {
-        return asset('/game_logo/'.$this->image);
-    }
-
-
 }
